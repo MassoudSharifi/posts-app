bd7e22792c797b9a0254bb6d9d91a2c4cdc3790e
MassoudSharifi <massoudf43@gmail.com>
2023-09-12 21:42:27 +0430

    Setup the libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Row, Col, List, Typography, Spin } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import "./styles/main.css";
+import axios from "axios";
 
+const { Title } = Typography;
 function App() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () =>
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts?userId=1")
+        .then((res) => res.data),
+  });
+  console.log(data);
+  if (isLoading || error) {
+    return (
+      <Row style={{ height: "100vh" }} justify="center" align="middle">
+        {isLoading ? <Spin size="large" /> : "An error has occurred: "}
+      </Row>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row justify="center">
+      <Col flex="600px" style={{ paddingBottom: "40px" }}>
+        <Title>Posts</Title>
+        <List
+          pagination={{
+            position: "bottom",
+            align: "end",
+            pageSize: 5,
+            showSizeChanger: false,
+          }}
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(
+            post: { title: string; id: string; body: string },
+            index
+          ) => (
+            <List.Item>
+              <List.Item.Meta
+                title={<a href="https://ant.design">{post.title}</a>}
+                description={post?.body}
+              />
+            </List.Item>
+          )}
+        />
+      </Col>
+    </Row>
   );
 }
 
