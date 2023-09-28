f3c6b12a98b8895e71a419ebb35a36771ad67c14
Massoud Sharifi <Massoudf43@gmail.com>
2023-09-28 02:11:56 -0700

    implement login with firbase phone number

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,70 @@
-import { Button, Select, Form, Input, Row, Col } from "antd";
+import { Button, Form, Input, Row, Col, message } from "antd";
 import { useNavigate } from "react-router-dom";
-import React from "react";
+import { useState } from "react";
+import { auth } from "../config/firbase";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
 type FieldType = {
-  username?: string;
-  userId?: string;
+  phoneNumber?: string;
+  code?: string;
 };
+
 export default function LoginPage() {
   const navigate = useNavigate();
-  const onFinish = (values: any) => {
-    localStorage.setItem("userData", JSON.stringify(values));
-    navigate("/posts");
-  };
+  const [showVerification, setShowVerification] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const userIds = React.useMemo(
-    () =>
-      new Array(10).fill(null).map((_, index) => ({
-        label: index + 1 + "",
-        value: index + 1,
-      })),
-    []
-  );
+  function onCaptchVerify() {
+    //@ts-ignore
+    if (!window.recaptchaVerifier) {
+      //@ts-ignore
+      window.recaptchaVerifier = new RecaptchaVerifier(auth, "sign-in-button", {
+        size: "invisible",
+        //@ts-ignore
+        callback: (response) => {
+          console.log(response);
+        },
+      });
+    }
+  }
+
+  function onSignUp(values: any) {
+    onCaptchVerify();
+    setLoading(true);
+    //@ts-ignore
+    const appVerifier = window.recaptchaVerifier;
+    signInWithPhoneNumber(auth, values?.phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        message.success("Successfully sent the Verification code");
+        //@ts-ignore
+        window.confirmationResult = confirmationResult;
+        setShowVerification(true);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+      });
+  }
+
+  function onOTPVerify(values: any) {
+    setLoading(true);
+    //@ts-ignore
+    window?.confirmationResult
+      .confirm(values.code)
+      .then(() => {
+        message.success("User signed in successfully.");
+        setLoading(false);
+        localStorage.setItem(
+          "userData",
+          JSON.stringify({ userName: "Massoud", userId: "1" })
+        );
+        navigate("/posts");
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }
 
   return (
     <Row style={styles.body} justify="center" align="middle">
@@ -32,31 +76,46 @@ export default function LoginPage() {
           borderRadius: "12px",
         }}
       >
+        <div id="sign-in-button"></div>
         <Form
           name="basic"
-          onFinish={onFinish}
+          onFinish={showVerification ? onOTPVerify : onSignUp}
           autoComplete="off"
           layout="vertical"
         >
-          <Form.Item<FieldType>
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: "Please enter your username!" }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item<FieldType>
-            label="User Id"
-            name="userId"
-            rules={[{ required: true, message: "Please select one user id" }]}
-          >
-            <Select options={userIds} />
-          </Form.Item>
-
+          {!showVerification ? (
+            <Form.Item<FieldType>
+              label="Phone number"
+              name="phoneNumber"
+              rules={[
+                { required: true, message: "Please enter your phone number!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          ) : (
+            <Form.Item<FieldType>
+              label="Verification Code"
+              name="code"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your verification code",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          )}
           <Form.Item>
-            <Button type="primary" htmlType="submit" block style={styles.login}>
-              Log in
+            <Button
+              type="primary"
+              htmlType="submit"
+              block
+              style={styles.login}
+              loading={loading}
+            >
+              {showVerification ? "SEND" : "Log in"}
             </Button>
           </Form.Item>
         </Form>
